877495b6083a18984007510a94db69ef045d4ea1
Barnabas Prosser <barnabas@barnabas.co.uk>
2025-05-04 11:20:45 +0100

    feat: add support for storing and printing chat history

--- chat-sim.c
+++ chat-sim.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+#define MAX_MESSAGES 100
+
 enum messageType {
- JOIN,
- LEAVE,
- MESSAGE   
+    JOIN,
+    LEAVE,
+    MESSAGE
 };
 
 struct Message {
@@ -12,28 +14,50 @@ struct Message {
     enum messageType type;
 };
 
+struct Message chat[MAX_MESSAGES];
+
+int counter = 0;
+
 void printMessage(struct Message *msg) {
     switch(msg->type) {
-        case JOIN:
-            printf("[%s] joined the chat\n",msg->sender);
-            break;
-        case LEAVE:
-            printf("[%s] left the chat\n",msg->sender);
-            break;
-        case MESSAGE:
-            printf("[%s]: %s\n",msg->sender,msg->content);
-            break;
-        default:
-            printf("Invalid msg\n");
-            break;
-        }
+    case JOIN:
+        printf("[%s] joined the chat\n",msg->sender);
+        break;
+    case LEAVE:
+        printf("[%s] left the chat\n",msg->sender);
+        break;
+    case MESSAGE:
+        printf("[%s]: %s\n",msg->sender,msg->content);
+        break;
+    default:
+        printf("Invalid msg\n");
+        break;
+    }
 }
 
 int main() {
-    struct Message msg = {"alice", "", JOIN};
-    printMessage(&msg);
-    struct Message msg2 = {"bob", "", LEAVE};
-    printMessage(&msg2);
-    struct Message msg3 = {"charlie", "Hello world!", MESSAGE};
-    printMessage(&msg3);
+    chat[counter++] = (struct Message) {
+        "Alice", "", JOIN
+    };
+
+    chat[counter++] = (struct Message) {
+        "Alice", "Hey Bob!", MESSAGE
+    };
+
+    chat[counter++] = (struct Message) {
+        "Bob", "", JOIN
+    };
+
+    chat[counter++] = (struct Message) {
+        "Bob", "Hey Alice!", MESSAGE
+    };
+
+    chat[counter++] = (struct Message) {
+        "Alice", "", LEAVE
+    };
+
+
+    for(int i = 0; i < counter; i++) {
+        printMessage(&chat[i]);
+    }
 }
